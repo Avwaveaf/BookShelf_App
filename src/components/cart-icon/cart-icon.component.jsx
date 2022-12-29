ab2bc844972f626ef956c7da317a04a6a0e1034d
fireCulex <5dillah01@gmail.com>
2022-12-29 18:31:47 +0700

    adding useOnClickOutside lib and implement it on icon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,16 @@
 import GoogleIcon from "../googleIcon/googleIcon.component";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { DropdownContext } from "../../contexts/dropdown.context";
+import  useOnClickOutside  from 'use-onclickoutside';
 import "./cart-icon.style.scss"
 const CartIcon = () => {
-    const { isOpen, setIsOpen,cartItems } = useContext(DropdownContext);
+    const { isOpen, setIsOpen, cartItems } = useContext(DropdownContext);
+    const containerRef = useRef(null);
     const toggleHandler = () =>{
         setIsOpen(!isOpen)
     }
-    return     <div className="nav-link-cart" onClick={toggleHandler}>
+    useOnClickOutside(containerRef, ()=>setIsOpen(false))
+    return     <div ref={containerRef} className="nav-link-cart" onClick={toggleHandler}>
     <GoogleIcon	Icon iconName="shopping_bag" style={{fontSize:"40px"}}/>
         <span className="cart_count" onClick={toggleHandler}>{ cartItems.length}</span>
 </div>
