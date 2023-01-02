87f00580da4afd85e6e40c8fea25e8cdaca0eb2f
fireCulex <5dillah01@gmail.com>
2023-01-02 14:07:01 +0700

    disabling onClickOutside listener and preparing for deploy

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,7 @@
 import GoogleIcon from "../googleIcon/googleIcon.component";
 import { useContext, useRef } from "react";
 import { DropdownContext } from "../../contexts/dropdown.context";
-import  useOnClickOutside  from 'use-onclickoutside';
+// import  useOnClickOutside  from 'use-onclickoutside';
 import "./cart-icon.style.scss"
 const CartIcon = () => {
     const { isOpen, setIsOpen, cartItems } = useContext(DropdownContext);
@@ -9,7 +9,7 @@ const CartIcon = () => {
     const toggleHandler = () =>{
         setIsOpen(!isOpen)
     }
-    useOnClickOutside(containerRef, ()=>setIsOpen(false))
+    // useOnClickOutside(containerRef, ()=>setIsOpen(false))
     return     <div ref={containerRef} className="nav-link-cart" onClick={toggleHandler}>
     <GoogleIcon	Icon iconName="shopping_bag" style={{fontSize:"40px"}}/>
         <span className="cart_count" onClick={toggleHandler}>{ cartItems.length}</span>
